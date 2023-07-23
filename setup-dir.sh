24daedc5fbc277d84b948a2fc877fd3cd3b73ab5
Turkin Nikolai <nikolai.turkin.main@gmail.com>
2023-07-23 18:26:50 +0300

    update

--- setup-dir.sh
+++ setup-dir.sh
@@ -2,7 +2,7 @@
 
 cd /media/ssd
 
-find . -name "output_images" -exec rm -dr {} \;
+find / -name "output_images" -exec rm -dr {} \;
 
 mkdir output_images && cd output_images
 grep -o '^[A-Z]* ' /home/pi/scripts/H8.PIPELINE/detection/rtps.txt | while read temp; do mkdir "$temp" ; done
